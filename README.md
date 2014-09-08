028bc6e35358c35918593e29d094ea5d618c0b9c
fxl <frank@frankledo.com>
2014-09-07 23:16:04 -0700

    First draft, current working state

---
+++ README.md
@@ -0,0 +1,66 @@
+# Getting and Cleaning Data: Course Project
+
+## Introduction
+
+This repository contains the course project for the Coursera course "Getting and Cleaning data", part of the Data Science specialization.
+
+The purpose of this project is to demonstrate the ability to collect, work
+with, and clean a data set. The goal is to prepare a tidy data set that can be
+used for later analysis.
+
+The project requires the following output:
+
+1. A tidy data set as described below
+
+1. This Github repository with the run_analysis.R script for performing the analysis
+
+1. A code book (CodeBook.md) that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md.
+
+1. This README.md file
+
+## Project
+
+One of the most exciting areas in all of data science right now is wearable
+computing - see for example this article: http://tinyurl.com/dsatatbftwtsb
+. Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most
+advanced algorithms to attract new users. The data linked to from the course
+website represent data collected from the accelerometers from the Samsung
+Galaxy S smartphone.
+
+### Data
+Here is the data for the project:
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+A full description of the data is available at the site where the
+data was obtained:  http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+## Project Goal
+
+The goal of the project is to output a tidy data set containing only the average of the mean and the standard deviations of each activity and each subject from both the training and test data included in the zip file referenced above.
+
+## How to use
+
+The run_analysis.R script can be run from within R Studio.
+
+The run_analysis.R script does the following:
+
+1. If the folder "UCI HAR Dataset" is not currently present in your current working directory, the data set zip file referenced above is downloaded and unzipped.
+
+1. Extracts only the measurements on the mean and standard deviation for each measurement from both the test and training sets.
+
+3. Uses descriptive activity names to name the activities in the data set
+
+4. Appropriately labels the data set with descriptive variable names.
+
+5. From the data set in step 4, creates a second, independent tidy dataset with the average of each variable for each activity and each subject.
+
+Sample output is included as: run_analysis.html
+
+### References
+
+Getting and Cleaning data course:
+	https://www.coursera.org/course/getdata
+
+Data Science Specialization:
+	https://www.coursera.org/specialization/jhudatascience/1
---
+++ run_analysis.R
@@ -0,0 +1,128 @@
+# Getting and Cleaning Data Course Project
+
+##### Configuration and setup
+## Uncomment and set if you prefer a different working directory
+#setwd("~/Desktop/Coursera/Data\ Science/3\ -\ Getting\ and\ Cleaning\ Data/Course\ Project/")
+input_data_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+output_file_name <- "tidy.txt"
+
+# Require/install prerequisites
+prerequisites <- function(){
+    if (!require("data.table")) {
+      install.packages("data.table")
+      require("data.table")
+
+    }
+    if (!require("reshape2")) {
+      install.packages("reshape2")
+      require("reshape2")
+    }
+}
+
+# Given a URL pointing to a zip file, checks to see if the file, or extracted
+# folder from the file exists in the current directory, and if not, downloads
+# and extracts it.  Returns the directory name.
+get_extract_file <- function(url){
+    file = basename(URLdecode(url))
+    datadir <- gsub(".zip", "", file)
+    if(!file.exists(datadir)) {
+        if(!file.exists(file)){
+            msg = sprintf("Downloading '%s' from %s...", file, url)
+            message(msg)
+            download.file(url=url, destfile=file, mode="w", method="curl")
+            if(!file.exists(file)){
+                message(sprintf("Error %s", msg))
+                quit(1)
+            } else {
+                # Keep a record of when this data was downloaded
+                fc <- file(sprintf("%s-date-downloaded.txt", datadir))
+                writeLines(date(), fc)
+                close(fc)
+            }
+        }
+        message("Unzipping file...")
+        unzip(file)
+    }
+    return(datadir)
+}
+
+# Extracts only the measurements on the mean and standard deviation for each
+# measurement from the test and train data sets, returning a merged data set.
+merge_data_sets <- function(datadir){
+    ## Requirements 1-4 are handled in this function.  For purposes of
+    ## efficiency, they are not handled in numeric order.
+
+    # Import features
+    features <- read.table(sprintf("%s/features.txt", datadir))[,2]
+    # Read activity labels
+    activity_labels <- read.table(sprintf("%s/activity_labels.txt", datadir),
+                                  col.names = c("id", "name"))
+
+    ## Requirement #2: Extracts only the measurements on the mean and standard
+    ## deviation for each measurement.
+    # Create vector of indices of columns containing 'mean' or 'std'.  We
+    # can use this later to filter the data for both data sets.
+    output_features <- grep("mean\\(|std\\(", features)
+
+    # Clean up feature names
+    #features <- sapply(features, gsub, pattern='\\(|\\)|,', replacement="")
+
+    # Loop through the test and train data sets.  For each data set, read the
+    # data from the 3 files (subject_%s.txt, X_%s.txt, and y_%s.txt), append
+    # the activity, names, and merge the 3 files into a single data frame
+    # named after the source data set.
+    for (source in c("test", "train")){
+        ## Requirement #4: Appropriately labels the data set with descriptive
+        ## variable names. This is satisfied by the col.names parameters to
+        ## read.table below
+        subject <- read.table(
+            sprintf("%s/%s/subject_%s.txt", datadir, source, source),
+            col.names = "subject_id")
+        data <- read.table(
+            sprintf("%s/%s/X_%s.txt", datadir, source, source),
+            col.names = features, check.names = FALSE)
+        activities <- read.table(
+            sprintf("%s/%s/y_%s.txt", datadir, source, source),
+            col.names = "activity_id")
+
+        ## Requirement #3: Uses descriptive activity names to name the
+        ## activities in the data set.
+        # Add activity names to activities data frame
+        activities$activity_name <- activity_labels$name[
+            match(activities$activity_id, activity_labels$id)]
+        # Create a variable named train or test that contains the frames
+        # above merged into a single frame
+        assign(source, cbind(activities, subject, data[,output_features]))
+    }
+    ## Requirement #1: Merges the training and the test sets to create one
+    ## data set.
+    return(rbind(train, test))
+}
+
+# Create tidy data set
+create_tidy_set <- function(datadir, output_file_name) {
+    # Requirement 5 is satisfied by this function
+
+    # First handle requirements 1-4...
+    data <- merge_data_sets(datadir)
+
+    ## Requirement #5: From the data set in step 4, creates a second,
+    ## independent tidy data set with the average of each variable for each
+    ## activity and each subject.
+
+
+    # Pivot the values of the data in to individual rows, keeping the
+    # identifier columns passed as id intact, resulting in a data frame
+    # containing the following columns:
+    #   "subject_id", "activity_id", "activity_name", "variable", "value"
+    pivoted_data <- melt(data, id = c(
+                         "subject_id", "activity_id", "activity_name"))
+    # Output the mean of all the values for each variable
+    tidy <- dcast(pivoted_data,
+                  subject_id + activity_id + activity_name ~ variable, mean)
+    # write the tidy data out to a file in the current working directory
+    write.table(tidy, file = output_file_name)
+}
+
+datadir <- get_extract_file(input_data_url)
+create_tidy_set(datadir, output_file_name)
